aaa05d0506e66349dc56732df508aac20465a563
bellisaidev <bellisai.dev@gmail.com>
2026-01-09 18:04:00 +0100

    feat: bootstrap backend with flyway and postgis schema

---
+++ backend/src/test/java/uk/fuelfinder/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.fuelfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
