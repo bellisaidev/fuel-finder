3a0966645d6fff6eb3104ba8484b75b5c8ab6b17
bellisaidev <bellisai.dev@gmail.com>
2026-03-25 18:47:55 +0100

    refactor: remove duplicated package structure and consolidate raw ingestion layout

--- backend/src/main/java/uk/co/fuelfinder/db/repo/LatestPriceRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.co.fuelfinder.db.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uk.co.fuelfinder.db.entity.LatestPriceEntity;
-import uk.co.fuelfinder.db.entity.LatestPriceId;
-
-public interface LatestPriceRepository extends JpaRepository<LatestPriceEntity, LatestPriceId> {}
--- backend/src/main/java/uk/co/fuelfinder/db/repo/RawFeedFetchRepository.java
+++
@@ -1,8 +0,0 @@
-package uk.co.fuelfinder.db.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uk.co.fuelfinder.db.entity.RawFeedFetchEntity;
-
-import java.util.UUID;
-
-public interface RawFeedFetchRepository extends JpaRepository<RawFeedFetchEntity, UUID> {}
