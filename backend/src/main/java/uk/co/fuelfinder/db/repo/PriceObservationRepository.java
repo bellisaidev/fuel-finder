bd2f05027b5c101b4c4d1d9d9fc80dec77c8d61b
bellisaidev <bellisai.dev@gmail.com>
2026-03-25 18:47:55 +0100

    refactor: remove duplicated package structure and consolidate raw ingestion layout

--- backend/src/main/java/uk/co/fuelfinder/db/repo/PriceObservationRepository.java
+++
@@ -1,8 +0,0 @@
-package uk.co.fuelfinder.db.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uk.co.fuelfinder.db.entity.PriceObservationEntity;
-
-import java.util.UUID;
-
-public interface PriceObservationRepository extends JpaRepository<PriceObservationEntity, UUID> {}
